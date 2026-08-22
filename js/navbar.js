84879a9b3879454cdb7eef3aea4ae1d0aa2292c7
RenzoRomo-dev <pedrorenzo@gmail.com>
2026-08-22 19:20:17 -0300

    agregado opcion navbar correccion de logica
    Co-authored-by: Nicolás Acosta <4costa2@users.noreply.github.com>

--- js/navbar.js
+++ js/navbar.js
@@ -32,6 +32,31 @@ navbar.innerHTML = `
                 Calcular pared
             </a>
 
+            <!-- select autodesplegable -->
+                <div class="relative group inline-block">
+
+  <!-- Botón principal -->
+  <button class="px-4 py-2 bg-black text-white rounded-md">
+    Opciones
+  </button>
+
+  <!-- Menú desplegable -->
+  <div class="absolute hidden group-hover:flex flex-col w-48 bg-black text-white border border-gray-800 rounded-md shadow-lg z-10 py-2">
+    <a href="/contrapiso/contrapiso.html" class="px-4 py-2 transition hover:text-sky-500 hover:bg-gray-900">
+      Calcular contrapiso
+    </a>
+
+    <a href="/techo/techo.html" class="px-4 py-2 transition hover:text-sky-500 hover:bg-gray-900">
+      Calcular techo
+    </a>
+
+    <a href="/pared/pared.html" class="px-4 py-2 transition hover:text-sky-500 hover:bg-gray-900">
+      Calcular pared
+    </a>
+  </div>
+
+</div> 
+
 
         </nav>
 
