47cf4631d3216b2bcccbfbf4556bf4898af0ec9e
RenzoRomo-dev <pedrorenzo@gmail.com>
2026-08-30 19:53:11 -0300

    cambio de nombre de calculadoras a presupuestos

--- js/navbar.js
+++ js/navbar.js
@@ -43,7 +43,7 @@ navbar.innerHTML = `
                     <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 text-sky-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M9 7h6m0 10v-3m-3 3h.01M9 17h.01M9 14h.01M12 14h.01M15 11h.01M12 11h.01M9 11h.01M7 21h10a2 2 0 002-2V5a2 2 0 00-2-2H7a2 2 0 00-2 2v14a2 2 0 002 2z" />
                     </svg>
-                    <span>Presupuestos</span>
+                    <span>Presupuestos disponibles</span>
                     <svg id="calcChevron" xmlns="http://www.w3.org/2000/svg" class="w-3.5 h-3.5 text-zinc-400 transition-transform duration-200" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M19 9l-7 7-7-7" />
                     </svg>
